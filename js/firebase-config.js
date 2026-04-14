fb424a393d356ba77f342c47c2a12e3034f184aa
kirgnaar <lavergne.kirian@gmail.com>
2026-04-14 11:13:13 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,21 +1,23 @@
 /**
  * CONFIGURATION FIREBASE
- * Remplacez les valeurs ci-dessous par celles de votre console Firebase
- * (Paramètres du projet > Vos applications > App Web)
+ * Mis à jour le 14/04/2026
  */
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "VOTRE_API_KEY",
+  apiKey: "AIzaSyCjsXZ7jvxD5RsuAfI19DvDpaACFpmUnPg",
   authDomain: "muscu-rpg.firebaseapp.com",
+  databaseURL: "https://muscu-rpg-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "muscu-rpg",
-  storageBucket: "muscu-rpg.appspot.com",
-  messagingSenderId: "VOTRE_SENDER_ID",
-  appId: "VOTRE_APP_ID"
+  storageBucket: "muscu-rpg.firebasestorage.app",
+  messagingSenderId: "137574537693",
+  appId: "1:137574537693:web:d225d84034efbd7d617e34",
+  measurementId: "G-BWF8H627RE"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
